a6ffc191f707cfd66b946e3cc96a4d2d8c5ba916
Cristina <Cristina@Cristina-TOSH>
2016-03-08 22:09:18 +0100

    Cuarta subida

---
+++ ProyectexaEntorno/src/Sumar.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Sumar {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("introduce un numero");
+		Scanner input = new Scanner(Sistem.in);
+		int x=NextInt;
+	}
+
+}
