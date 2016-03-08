1864812d4a3c40b3e9c99d6a3ae962305998c5c6
Cristina <Cristina@Cristina-TOSH>
2016-03-08 21:58:44 +0100

    Tercera subida

--- ProyectexaEntorno/src/MultiplicarSumar2.java
+++ ProyectexaEntorno/src/MultiplicarSumar2.java
@@ -1,10 +1,10 @@
 
-public class MultiplicarSumar {
+public class MultiplicarSumar2 {
 	
 		public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Nueva prueba");	
-			
+		System.out.println(" segunda prueba");	
 		int suma =0, producto =1;
 		for (int i=1;i<=10;i++)
 		{
